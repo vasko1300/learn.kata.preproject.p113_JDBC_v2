1a8587ef3b71b3a1d24d59fd2d72fb8e529a6f41
vasko1300 <miners.sheds5r@icloud.com>
2026-03-30 18:52:50 +0800

    chore: add Driver registration

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,6 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    // В JDBC версии младше 4.0 регистрация драйвера обязательна. С 4.0 подхватывается автоматом из classpath
+    // Драйвер необходимо зарегистрировать один раз до вызова getConnection - при загрузке класса. Поэтому в static блоке
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Unable to register driver", e);
+        }
+    }
     private static String url = "jdbc:mysql://localhost:3306/preproject113";
     private static String user = "root";
     private static String password = "132435";
